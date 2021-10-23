df4a990d160f9bad35cc3fb1ecde6cd7079accc0
Aashish Kumar <91531431+aashishkumar1@users.noreply.github.com>
2021-10-23 12:30:33 +0530

    update
    created food app

---
+++ Food app/navbar.js
@@ -0,0 +1,14 @@
+function navbar(){
+
+    return ` 
+    <input  type="text" id="food" placeholder="Enter food name" />
+
+    <div id="options">
+    <h3><a href="Home.html">Home</a></h3>
+    <h3><a href="Receipe.html">Today's Receipe</a></h3>
+    <h3><a href="Latest.html">Latest Receipe</a></h3>
+    
+  </div>`
+}
+
+export default navbar;
